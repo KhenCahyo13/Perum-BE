51dab8f0a703412e73a420612b8300200cc94f5d
KhenCahyo13 <khencahyo02@gmail.com>
2026-06-11 20:03:12 +0700

    ci: change nwidart stubs

--- Modules/Core/app/Http/Controllers/Controller.php
+++ Modules/Core/app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace Modules\Core\Http\Controllers;
 
 abstract class Controller
 {
---
+++ Modules/Core/app/Services/Service.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Modules\Core\Services;
+
+abstract class Service {}
